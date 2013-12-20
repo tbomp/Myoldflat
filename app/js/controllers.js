64854132fc41f186b4a462dc83e360776e9bd0b2
Tom Bomp <thomas.bompaire@yahoo.fr>
2013-12-20 18:09:52 +0100

    liste, design et ajout

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,7 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-   .controller('HomeCtrl', ['$scope', 'syncData', function($scope, syncData) {
-      syncData('syncedValue').$bind($scope, 'syncedValue');
-   }])
+
 
   .controller('ChatCtrl', ['$scope', 'syncData', function($scope, syncData) {
       $scope.newMessage = null;
@@ -22,100 +20,3 @@ angular.module('myApp.controllers', [])
          }
       };
    }])
-
-   .controller('LoginCtrl', ['$scope', 'loginService', '$location', function($scope, loginService, $location) {
-      $scope.email = null;
-      $scope.pass = null;
-      $scope.confirm = null;
-      $scope.createMode = false;
-
-      $scope.login = function() {
-         $scope.err = null;
-         if( !$scope.email ) {
-            $scope.err = 'Please enter an email address';
-         }
-         else if( !$scope.pass ) {
-            $scope.err = 'Please enter a password';
-         }
-         else {
-            loginService.login($scope.email, $scope.pass, function(err, user) {
-               $scope.err = err? err + '' : null;
-               if( !err ) {
-                  $location.replace();
-                  $location.path('/account');
-               }
-            });
-         }
-      };
-
-      $scope.createAccount = function() {
-         if( !$scope.email ) {
-            $scope.err = 'Please enter an email address';
-         }
-         else if( !$scope.pass ) {
-            $scope.err = 'Please enter a password';
-         }
-         else if( $scope.pass !== $scope.confirm ) {
-            $scope.err = 'Passwords do not match';
-         }
-         else {
-            loginService.createAccount($scope.email, $scope.pass, function(err, user) {
-               if( err ) {
-                  $scope.err = err? err + '' : null;
-               }
-               else {
-                  // must be logged in before I can write to my profile
-                  $scope.login(function(err) {
-                     if( !err ) {
-                        loginService.createProfile(user.uid, user.email);
-                     }
-                  });
-               }
-            });
-         }
-      };
-   }])
-
-   .controller('AccountCtrl', ['$scope', 'loginService', 'syncData', '$location', function($scope, loginService, syncData, $location) {
-      syncData(['users', $scope.auth.user.uid]).$bind($scope, 'user');
-
-      $scope.logout = function() {
-         loginService.logout();
-         $location.path('/login');
-      };
-
-      $scope.oldpass = null;
-      $scope.newpass = null;
-      $scope.confirm = null;
-
-      $scope.reset = function() {
-         $scope.err = null;
-         $scope.msg = null;
-      };
-
-      $scope.updatePassword = function() {
-         $scope.reset();
-         loginService.changePassword(buildPwdParms());
-      };
-
-      function buildPwdParms() {
-         return {
-            email: $scope.auth.user.email,
-            oldpass: $scope.oldpass,
-            newpass: $scope.newpass,
-            confirm: $scope.confirm,
-            callback: function(err) {
-               if( err ) {
-                  $scope.err = err;
-               }
-               else {
-                  $scope.oldpass = null;
-                  $scope.newpass = null;
-                  $scope.confirm = null;
-                  $scope.msg = 'Password updated!';
-               }
-            }
-         }
-      }
-
-   }]);
