adc7db2d89597953ecf1a89f1e81cf073ed76970
Tom Bomp <thomas.bompaire@yahoo.fr>
2014-01-09 10:04:12 +0100

    fonctionne parfaitement

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,7 +6,7 @@ angular.module('myApp.controllers', [])
 
 
   .controller('ChatCtrl', ['$scope', 'syncData', function($scope, syncData) {
-      $scope.newMessage = null;
+      $scope.newMessage = {};
 
       // constrain number of messages by limit into syncData
       // add the array into $scope.messages
@@ -15,7 +15,7 @@ angular.module('myApp.controllers', [])
       // add new messages to the list
       $scope.addMessage = function() {
          if( $scope.newMessage ) {
-            $scope.messages.$add({text: $scope.newMessage});
+            $scope.messages.$add({text: $scope.newMessage.text, description: $scope.newMessage.description});
             $scope.newMessage = null;
          }
       };
